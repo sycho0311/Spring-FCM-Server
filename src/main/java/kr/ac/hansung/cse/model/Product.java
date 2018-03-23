4a5cbb5e72475edf483d582c43bb7f0f59a03a12
sycho <sycho0311@gmail.com>
2018-03-23 16:09:02 +0900

    Adjust Product

--- src/main/java/kr/ac/hansung/cse/model/Product.java
+++ src/main/java/kr/ac/hansung/cse/model/Product.java
@@ -1,9 +1,12 @@
 package kr.ac.hansung.cse.model;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+@Component
 @Getter
 @Setter
 @ToString
